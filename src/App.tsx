96941f80afca86bad601d94ca004bd74e4c684fd
Anna Paios <annapaios90@gmail.com>
2026-02-28 17:13:00 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="font-['Inter-Bold']">
+        DBS booking is coming...
+      </div>
+    </>
+  )
+}
+
+export default App
