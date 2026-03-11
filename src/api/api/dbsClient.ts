459892db683b4f17cd5d96f80c70a98092b6b94a
Anna Paios <annapaios90@gmail.com>
2026-03-11 16:00:27 +0200

    fix vercel proxy

--- src/api/api/dbsClient.ts
+++ src/api/api/dbsClient.ts
@@ -1,11 +1,11 @@
 // dbsClient.ts
 import axios from "axios";
 
-const DBS_API =
-  import.meta.env.MODE === "development"
-    ? "/dbs"
-    : "https://dbs.roapp.page";
+// const DBS_API =
+//   import.meta.env.MODE === "development"
+//     ? "/dbs"
+//     : "https://dbs.roapp.page";
 
 export const dbsClient = axios.create({
-  baseURL: DBS_API
+  baseURL: "/dbs",
 });
