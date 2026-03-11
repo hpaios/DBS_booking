b8e611e3b9a25e7e5e9e7b83770d6ada3f52fc6d
Anna Paios <annapaios90@gmail.com>
2026-03-11 15:42:52 +0200

    fix build path

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig, loadEnv } from "vite";
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    base: '/DBS_booking/',
+    plugins: [react()],
+    base: "/",
     server: {
       proxy: {
         // 🔵 Публічний API (з токеном)
