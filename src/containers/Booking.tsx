619c3faa23aabd5dcce05a1637e3da0df4c11049
Anna Paios <annapaios90@gmail.com>
2026-03-18 18:33:20 +0200

    fixed fonts on mobile

--- src/containers/Booking.tsx
+++ src/containers/Booking.tsx
@@ -91,8 +91,8 @@ const Booking = () => {
 
   return (
     <div className="bg-[var(--color-bg-secondary)] font-['Inter'] p-[1.5rem] rounded-[var(--radius-lg)] mt-[-10px] relative min-h-[480px]">
-      <h1 className='text-[var(--color-icon)] text-center'>DBS | Technické centrum & Detailing</h1>
-      <h3 className='text-[var(--color-icon)] flex items-center gap-[0.5rem] justify-center'>
+      <h1 className='text-[var(--color-icon)] text-center font-[Inter]'>DBS | Technické centrum & Detailing</h1>
+      <h3 className='text-[var(--color-icon)] flex items-center gap-[0.5rem] justify-center font-[Inter]'>
         <LocationIcon width={"16"} height={"16"}/>
         <div>Františka Kadlece 2441, 180 00 Praha 8-Libeň</div>
       </h3>
