2940a154671aedbfaf321f7e8d507bcfffe15e10
Anna Paios <annapaios90@gmail.com>
2026-03-23 10:56:19 +0200

    added new bg

--- src/components/ContainerWrapper/ContainerWrapper.tsx
+++ src/components/ContainerWrapper/ContainerWrapper.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import bgImage from "../../assets/bg.avif";
+import bgImage from "../../assets/bgBooking.png";
 import logo from "../../assets/logo.png";
 import { HeaderStyle, HeaderWrapper, Logo } from './ContainerWrapper.style'
 
@@ -9,10 +9,10 @@ interface ContainerWrapperProps {
 
 const ContainerWrapper: React.FC<ContainerWrapperProps> = ({ children }) => {
   return (
-     <div
+    <div
       className={HeaderWrapper}
     >
-       <div className="absolute inset-0 bg-gradient-to-b to-transparent via-transparent from-[color:var(--color-bg)]/70 h-[120px] z-5"></div>
+      <div className="absolute inset-0 bg-gradient-to-b to-transparent via-transparent from-[color:var(--color-bg)]/70 h-[120px] z-5"></div>
       <header
         className={HeaderStyle}
         style={{ backgroundImage: `url(${bgImage})` }}
